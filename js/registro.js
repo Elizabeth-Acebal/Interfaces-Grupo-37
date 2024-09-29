f451995f7b23455e574fb62c8b8ce9f2c0c55bf2
Elizabeth-Acebal <elyacebal26@gmail.com>
2024-09-28 22:31:34 -0300

    Añadi forms, header y footer

---
+++ js/registro.js
@@ -0,0 +1,13 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que se envíe el formulario automáticamente
+
+    // Agregar la clase 'submitted' al botón
+    const button = document.querySelector('button');
+    button.classList.add('submitted');
+
+    // Simular la creación de la cuenta y quitar la clase después de la animación
+    setTimeout(function() {
+        button.classList.remove('submitted');
+    
+    }, 800); // Duración de la animación
+});
