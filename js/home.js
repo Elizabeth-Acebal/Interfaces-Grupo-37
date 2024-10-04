bb9f4da7fa45eeae0e0b9902315cee380e1dad27
Elizabeth-Acebal <elyacebal26@gmail.com>
2024-10-04 14:29:28 -0300

    Agregue menu perfil

--- js/home.js
+++ js/home.js
@@ -9,3 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 })
 
+//div perfil
+document.getElementById('img-perfil').addEventListener('click', function() {
+    const perfilContainer = document.getElementById('container-perfil');
+    perfilContainer.classList.toggle('visible'); // Alterna la clase 'visible'
+});
