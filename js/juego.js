43e279289baef98dcafc2412b4e454709a366558
Elizabeth-Acebal <elyacebal26@gmail.com>
2024-10-09 11:11:48 -0300

    caja de comentarios

--- js/juego.js
+++ js/juego.js
@@ -1,5 +1,25 @@
 "use strict";
 
-
+document.querySelector('.send-button').addEventListener('click', () => {
+    const commentText = document.querySelector('.comment-input-box textarea').value;
+    if (commentText.trim() !== '') {
+      const newComment = document.createElement('article');
+      newComment.classList.add('comment');
+      newComment.innerHTML = `
+        <img src="user-avatar.jpg" alt="Avatar de usuario" class="comment-avatar">
+        <div class="comment-content">
+          <p class="comment-author">Tú</p>
+          <p class="comment-text">${commentText}</p>
+          <div class="comment-actions">
+            <span class="like">👍</span>
+            <span class="dislike">👎</span>
+            <a href="#" class="reply">responder</a>
+          </div>
+        </div>
+      `;
+      document.querySelector('.comments-list').appendChild(newComment);
+      document.querySelector('.comment-input-box textarea').value = '';
+    }
+  });
 
 
